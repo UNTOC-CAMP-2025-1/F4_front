72676eebe1002e5756fd5e5d7b02792e42b80490
김현지 <hyunji051127@gmail.com>
2025-05-07 00:55:25 +0900

    화면 크기 설정

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ import Start from './scenes/Start.js';
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: [Start]
+    width: window.innerWidth,
+    height: window.innerHeight,
+    scene: [Start],
+    scale: {
+        mode: Phaser.Scale.RESIZE,
+        autoCenter: Phaser.Scale.CENTER_BOTH //가운데 정렬
+    }
 };
 
 new Phaser.Game(config);
