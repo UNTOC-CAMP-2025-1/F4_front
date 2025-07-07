f4d95c74d51083ddd8246f309dfea532d2ed52ea
김현지 <hyunji051127@gmail.com>
2025-07-08 03:24:30 +0900

    방향키 수정

--- scenes/entities/PlayerSnake.js
+++ scenes/entities/PlayerSnake.js
@@ -1,80 +1,71 @@
-// src/scenes/entities/PlayerSnake.js
 import Snake from './Snake.js';
 
-/**
- * Player-controlled snake, extending the core Snake class.
- * @extends Snake
- */
 export default class PlayerSnake extends Snake {
-  /**
-   * @param {Phaser.Scene} scene      현재 씬(this)
-   * @param {string} sectionKey       몸통(섹션) 스프라이트 키
-   * @param {string} headKey          머리 스프라이트 키
-   * @param {number} x                초기 X 좌표
-   * @param {number} y                초기 Y 좌표
-   */
   constructor(scene, sectionKey, headKey, x, y) {
     super(scene, sectionKey, headKey, x, y);
 
     this.head.setScale(0.4);
 
-    // 화살표 키 입력용
     this.cursors = scene.input.keyboard.createCursorKeys();
+    this.keys = scene.input.keyboard.addKeys({
+      A: Phaser.Input.Keyboard.KeyCodes.A,
+      D: Phaser.Input.Keyboard.KeyCodes.D,
+      SPACE: Phaser.Input.Keyboard.KeyCodes.SPACE,
+    });
 
-    // 스페이스바로 가속/감속 제어
+    this.currentSpeed = this.slowSpeed;  // 자동 전진 초기값
+    this.maxSpeed = this.slowSpeed;
+    this.acceleration = 300;             // 부드럽게 속도 바뀌도록
+    this.turnSpeed = 2.5;                // 라디안/초
+
     const spaceKey = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     spaceKey.on('down', this.spaceKeyDown, this);
-    spaceKey.on('up',   this.spaceKeyUp,   this);
+    spaceKey.on('up', this.spaceKeyUp, this);
 
-    // 객체 파괴 시 이벤트 제거
     this.addDestroyedCallback(() => {
       spaceKey.off('down', this.spaceKeyDown, this);
-      spaceKey.off('up',   this.spaceKeyUp,   this);
+      spaceKey.off('up', this.spaceKeyUp, this);
     }, this);
   }
 
-  /** 가속 처리 */
   spaceKeyDown() {
-    this.speed = this.fastSpeed;
+    this.maxSpeed = this.fastSpeed;
     this.shadow.isLightingUp = true;
   }
 
-  /** 감속 처리 */
   spaceKeyUp() {
-    this.speed = this.slowSpeed;
+    this.maxSpeed = this.slowSpeed;
     this.shadow.isLightingUp = false;
   }
 
-  /**
-   * 매 프레임 호출되는 콜백
-   * @param {number} time
-   * @param {number} delta
-   */
   update(time, delta) {
-    // 1) 부모 클래스의 길이 증가 등 내부 로직 실행
     super.update(time, delta);
+    const dt = delta / 1000;
 
-    // 2) 마우스 위치 계산
-    const pointer = this.scene.input.activePointer;
-    const mouseX = pointer.worldX;
-    const mouseY = pointer.worldY;
+    const turnLeft = this.cursors.left.isDown || this.keys.A.isDown;
+    const turnRight = this.cursors.right.isDown || this.keys.D.isDown;
 
-    // 3) 머리 스프라이트 위치
-    const headX = this.head.x;
-    const headY = this.head.y;
+    // 회전 입력 처리
+    if (turnLeft) {
+      this.head.rotation -= this.turnSpeed * dt;
+    }
+    if (turnRight) {
+      this.head.rotation += this.turnSpeed * dt;
+    }
 
-    // 4) 목표 각도(도 단위) 계산
-    let angleDeg = Phaser.Math.RadToDeg(
-      Math.atan2(mouseY - headY, mouseX - headX)
-    );
+    // 현재 속도를 maxSpeed에 부드럽게 맞추기
+    const speedDiff = this.maxSpeed - this.currentSpeed;
+    const accelStep = this.acceleration * dt;
+    if (Math.abs(speedDiff) < accelStep) {
+      this.currentSpeed = this.maxSpeed;
+    } else {
+      this.currentSpeed += Math.sign(speedDiff) * accelStep;
+    }
 
-    // 5) 시각적 회전 적용
-    this.head.setAngle(angleDeg);
-
-    // 6) 속도 벡터 설정 (this.speed는 spaceKeyDown/Up에서 설정됨)
-    this.scene.physics.velocityFromAngle(
-      angleDeg,
-      this.speed,
+    // 현재 회전 방향으로 이동
+    this.scene.physics.velocityFromRotation(
+      this.head.rotation - Math.PI / 2,
+      this.currentSpeed,
       this.head.body.velocity
     );
   }
